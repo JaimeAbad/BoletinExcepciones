5773145b160d50bd828e71fdd48093e629496bcc
JaimeAF <aquileseeb@gmail.com>
2018-11-16 11:49:40 +0100

    Ejercicio3
    Empezando.
    Sacar formula para saber cuando un numero es primo

---
+++ src/Ej03.java
@@ -0,0 +1,28 @@
+
+public class Ej03 {
+
+	public static void main(String[] args) {
+
+		
+		/*Se dice que un número entero es primo si sólo es divisible entre 1 y entre sí mismo. Por 
+ejemplo, 2, 3, 5 y 7 son primos, pero 4, 6, 8 y 9 no lo son. 
+a)
+Escribe una función que determine si un número es primo o no.
+b)
+Utiliza esta función en un programa que muestre todos los números primos entre 1 y 
+10.000. */
+	}
+	
+	
+	public static boolean esPrimo(int numero) {
+		boolean primo = false;
+		
+		
+		
+		
+		
+		
+		return primo;
+	}
+
+}
