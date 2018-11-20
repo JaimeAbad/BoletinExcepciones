5cd5cf9a8fcef3766dd5fc9d1ec700b3a4b18dd8
JaimeAF <aquileseeb@gmail.com>
2018-11-20 09:33:38 +0100

    Ej3
    terminado

--- src/Ej03.java
+++ src/Ej03.java
@@ -1,4 +1,3 @@
-
 public class Ej03 {
 
 	public static void main(String[] args) {
@@ -11,16 +10,58 @@ Escribe una función que determine si un número es primo o no.
 b)
 Utiliza esta función en un programa que muestre todos los números primos entre 1 y 
 10.000. */
+		
+		/*para saber si un numero es primo, lo dividimos desde el 2 hasta n-1,
+		 * si nigun cociente de esas divisiones da exacto (exacto  = que el resto sea 0), el numero es primo
+		 * 
+		 * */
+		//Scanner keyboard = new Scanner(System.in);
+		final int VALOR = 10000;
+		//float numero;
+		boolean ser;
+		
+//		System.out.println("Introduce el numero que quieres comprobar si es primo: ");
+//		numero = keyboard.nextFloat();
+//		keyboard.close();
+//		ser = esPrimo(numero);
+//		
+//		if(ser == true) {
+//			System.out.println("Este numero es primo");
+//		}
+//		else {
+//			System.out.println("Este numero no es primo");
+//		}
+		
+		//no empieza desde el 1 pq la consola tiene un tamaño maximo
+		for(int j = 1 ;j <=VALOR;j++) {
+			ser = esPrimo(j);
+			
+			System.out.print(j);
+			if(ser) {
+				System.out.println("Este numero es primo");
+			}
+			else {
+				System.out.println("Este numero no es primo");
+			}
+		}
+		
+		
 	}
 	
 	
-	public static boolean esPrimo(int numero) {
-		boolean primo = false;
+	public static boolean esPrimo(float numero) {
+		boolean primo = true;
+		float i;
 		
-		
-		
-		
-		
+		for(i = 2.0f; i<numero ; i ++) {
+			
+			if(numero%i == 0) {
+				i = numero;
+				primo = false;
+			}
+			
+		}
+
 		
 		return primo;
 	}
